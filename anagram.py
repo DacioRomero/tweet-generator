30f4231d64c311229ec08fa630fb98b71664d38d
Dacio Romero <dacioromero@gmail.com>
2018-10-25 10:18:53 -0700

    Add anagram MVP

---
+++ anagram.py
@@ -0,0 +1,14 @@
+# Display an anagram of a provided word
+import random
+
+
+# TODO: Generate *real* anagrams
+def generator(word):
+    word_list = list(word)
+    random.shuffle(word_list)
+    return ''.join(word_list)
+
+
+if __name__ == '__main__':
+    import sys
+    print(generator(sys.argv[1]))
