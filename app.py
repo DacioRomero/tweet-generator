32baca63b33bed4fe94e1462756766d511d9aab6
Dacio Romero <dacioromero@gmail.com>
2018-11-07 22:02:53 -0800

    Read text outside of app for performance

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask
 import sample
 import histogram
+import os
 
 app = Flask(__name__)
+
+with open('./texts/test.txt') as file:
+    my_text = file.read()
+
+my_histogram = histogram.generate_histogram_dict(my_text)
 
 @app.route("/")
 def random_sentence():  #
     '''Root route for webapp'''
-    with open('./texts/test.txt') as file:
-        my_text = file.read()
-
-    my_histogram = histogram.generate_histogram_dict(my_text)
     return ' '.join(sample.histogram_samples(my_histogram, k=25))
-
-if __name__ == '__main__':
-    app.run(debug=True)
