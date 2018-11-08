7b5fe8259984c7dc48bca7db9db10aebef241ef0
Dacio Romero <dacioromero@gmail.com>
2018-11-08 13:07:09 -0800

    Separate functions into submodules

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask
-import sample
+import sys
+sys.path.insert(0, 'coursework/Code')
+sys.path.insert(0, 'coursework/Code/Python-Tools')
+from dictogram import Dictogram
 import histogram
 
 app = Flask(__name__)
 
-with open('./texts/test.txt') as file:
+with open('./corpus.txt') as file:
     my_text = file.read()
 
-my_histogram = histogram.generate_histogram_dict(my_text)
+my_dictogram = Dictogram(histogram.get_words(my_text))
 
 @app.route("/")
 def random_sentence():  #
     '''Root route for webapp'''
-    return ' '.join(sample.histogram_samples(my_histogram, k=25))
+    return ' '.join(my_dictogram.samples(25))
