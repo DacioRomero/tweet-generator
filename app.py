70775f59f97b51d52efd22bec538b7fb4c2c61fc
Dacio Romero <dacioromero@gmail.com>
2018-11-07 16:59:31 -0800

    Add Flask debugging

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import histogram
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def random_sentence():  #
+    '''Root route for webapp'''
     with open('./texts/test.txt') as file:
         my_text = file.read()
 
     my_histogram = histogram.generate_histogram_dict(my_text)
     return ' '.join(sample.histogram_samples(my_histogram, k=25))
+
+if __name__ == '__main__':
+    app.run(debug=True)
