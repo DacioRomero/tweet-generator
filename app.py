ec8fe96e9685e4c9606d5a799bd63f47969953ea
Dacio Romero <dacioromero@gmail.com>
2018-12-04 13:15:35 -0800

    Fix app

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask
 import sys
-sys.path.insert(0, 'coursework/Code')
-sys.path.insert(0, 'coursework/Code/Python-Tools')
-from dictogram import Dictogram
+from flask import Flask
+sys.path.insert(0, './coursework/Code')
 import histogram
+# from dictogram import Dictogram
+from listogram import Listogram
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-with open('./corpus.txt') as file:
-    my_text = file.read()
+    with open('./corpus.txt') as file:
+        my_text = file.read()
 
-my_dictogram = Dictogram(histogram.get_words(my_text))
+    # app.histogram = Dictogram(histogram.get_words(my_text))
+    app.histogram = Listogram(histogram.get_words(my_text))
 
-@app.route("/")
-def random_sentence():  #
-    '''Root route for webapp'''
-    return ' '.join(my_dictogram.samples(25))
+    @app.route("/")
+    def random_sentence():  #
+        return ' '.join(app.histogram.samples(25))
+
+    return app
